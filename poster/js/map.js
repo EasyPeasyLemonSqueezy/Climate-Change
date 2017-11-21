6b327cbcf58fa94c03dd343234a1c5c7dae0fa34
Learko <matveevantonekb@gmail.com>
2017-11-21 22:46:58 +0500

    add cache + refactoring

--- poster/js/map.js
+++ poster/js/map.js
@@ -1,47 +1,29 @@
 let currentYear = 1990;
 
+let cache = {};
+let map;
 
-function drawMap(countries, codes) {
-    var dataset = {};
-    
-    var onlyValues = [];
-    for (var key in countries) {
-        onlyValues.push(countries[key][currentYear - 1990]);
-    }
 
-    var minValue = Math.min.apply(null, onlyValues),
-        maxValue = Math.max.apply(null, onlyValues);
+function initMap(countries, codes, callback) {
+    document.getElementById("map-container").innerHTML = '';
 
-    var paletteScale = d3.scale.log()
-        .domain([minValue,maxValue])
-        .range(["#d3d3d3", "#1F1F1F"]); 
-
-    for (var key in countries){
-        var value = countries[key][currentYear - 1990];
-        dataset[codes[key]] = { numberOfThings: Math.round(value / 1000), fillColor: paletteScale(value) };
-    }
-
-    document.getElementById("map-container").innerHTML = "";
-
-    new Datamap({
+    map = new Datamap({
         element: document.getElementById('map-container'),
         projection: 'mercator',
         fills: { defaultFill: '#F5F5F5' },
-        data: dataset,
         geographyConfig: {
-           borderColor: '#DEDEDE',
-           highlightBorderWidth: 2,
-           highlightFillColor: function(geo) {
-               return geo['fillColor'] || '#F5F5F5';
-           },
-           highlightBorderColor: '#B7B7B7',
-           popupTemplate: function(geo, data) {
-               if (!data) { return ; }
-               return ['<div class="hoverinfo">',
-                   '<strong>', geo.properties.name, '</strong>',
-                   '<br>CO2(Gkg): <strong>', data.numberOfThings, '</strong>',
-                   '</div>'].join('');
-           }
+            borderColor: '#DEDEDE',
+            highlightBorderWidth: 2,
+            highlightFillColor: (geo) => geo['fillColor'] || '#F5F5F5',
+            highlightBorderColor: '#B7B7B7',
+            popupTemplate: (geo, data) => {
+                if (!data) { return; }
+                
+                return ['<div class="hoverinfo">',
+                        '<strong>', geo.properties.name, '</strong>',
+                        '<br>CO2(Gkg): <strong>', data.numberOfThings, '</strong>',
+                        '</div>'].join('');
+            }
         },
         setProjection: function (element) {
             let projection = d3.geo.mercator()
@@ -52,7 +34,48 @@ function drawMap(countries, codes) {
             return { path: path, projection: projection };
         }
     });
+
+
+    drawMap(countries, codes);
+
+    callback();
 }
+
+
+function drawMap(countries, codes) {
+    if (currentYear in cache) {
+        map.data = cache[currentYear];
+    }
+    else {
+        let dataset = {};
+        let onlyValues = [];
+
+        for (let key in countries) {
+            onlyValues.push(countries[key][currentYear - 1990]);
+        }
+
+        let minValue = Math.min.apply(null, onlyValues),
+            maxValue = Math.max.apply(null, onlyValues);
+
+        let paletteScale = d3.scale.log()
+            .domain([minValue, maxValue])
+            .range(["#d3d3d3", "#1F1F1F"]);
+
+        for (let key in countries){
+            let value = countries[key][currentYear - 1990];
+
+            dataset[codes[key]] = {
+                numberOfThings: Math.round(value / 1000),
+                fillColor: paletteScale(value)
+            };
+        }
+
+        cache[currentYear] = dataset;
+    }
+
+    map.updateChoropleth(cache[currentYear]);
+}
+
 
 function startMap(countries, codes) {
     timer = setInterval(() => {
@@ -75,7 +98,8 @@ function startMap(countries, codes) {
 
 $.getJSON("/data/country_code.json", (codes) => {
     $.getJSON("/data/co2e.min.json", (countries) => {
-        drawMap(countries, codes);
-        startMap(countries, codes);
+        initMap(countries, codes,
+            () => startMap(countries, codes)
+        );
     });
 });
